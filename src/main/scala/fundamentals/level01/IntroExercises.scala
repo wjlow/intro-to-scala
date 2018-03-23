a17e943fc95c6c5b853c40048a8ec58b5f5a6a5e
Jack Low <jack.low@rea-group.com>
2018-03-23 14:20:15 +1100

    Add function currying

--- src/main/scala/fundamentals/level01/IntroExercises.scala
+++ src/main/scala/fundamentals/level01/IntroExercises.scala
@@ -21,6 +21,23 @@ object IntroExercises {
   def add(x: Int, y: Int): Int = ???
 
   /**
+    * Currying
+    *
+    * What if we want to define a function `add5` that adds 5 to any `Int`?
+    *
+    * scala> add5(10)
+    * = 15
+    *
+    * scala> add5(7)
+    * = 12
+    *
+    * Hint: Use curriedAdd
+    */
+  def curriedAdd(x: Int)(y: Int): Int = ???
+
+  def add5(y: Int): Int = ???
+
+  /**
     * Parametric types
     *
     * How many ways can you implement this function?
