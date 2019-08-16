784f499138906770cd0939bf6fab74da3871a98c
Jack Low <jack.low@rea-group.com>
2019-08-16 16:02:56 +1000

    Fill out Option3 with flatMap solutions, so it's more obvious how to change it to a for comprehension.

--- src/main/scala/introcourse/level04/OptionExercises3.scala
+++ src/main/scala/introcourse/level04/OptionExercises3.scala
@@ -1,6 +1,6 @@
 package introcourse.level04
 
-import introcourse.level04.OptionExercises2.{HumanId, Job, JobId, findHumanById, findJobById}
+import introcourse.level04.OptionExercises2.{HumanId, Job, JobId, findHumanById, findJobById, findJobIdByHumanId, findJobByHumanId}
 
 /**
   * These exercises mirror the ones from `OptionExercises2.scala`,
@@ -9,38 +9,35 @@ import introcourse.level04.OptionExercises2.{HumanId, Job, JobId, findHumanById,
 object OptionExercises3 {
 
   /**
+    * Rewrite this function using for-comprehension syntax.
+    *
     * scala> findJobIdByHumanIdUsingFor(1)
     * = None
     *
     * scala> findJobIdByHumanIdUsingFor(2)
     * = Some(1)
     */
-  def findJobIdByHumanIdUsingFor(humanId: HumanId): Option[JobId] = ???
-//    for {
-//      human <- ??? // Find a function of type HumanId => Option[Human]
-//      jobId <- ??? // Find a function of type Human => Option[JobId]
-//    } yield jobId
+  def findJobIdByHumanIdUsingFor(humanId: HumanId): Option[JobId] =
+    findHumanById(humanId).flatMap(human => human.optJobId)
 
   /**
+    * Rewrite this function using for-comprehension syntax.
+    *
     * scala> findJobByHumanIdUsingFor(2)
     * = Some(Job("Teacher", "Expert in their field"))
-    *
-    * Hint: Use findJobIdByHumanIdUsingFor
     */
-  def findJobByHumanIdUsingFor(humanId: HumanId): Option[Job] = ???
-//    for {
-//      jobId <- ??? // Find a function of type HumanId => Option[JobId]
-//      job <- ???   // Find a function of type JobId => Option[Job]
-//    } yield job
+  def findJobByHumanIdUsingFor(humanId: HumanId): Option[Job] =
+    findJobIdByHumanId(humanId).flatMap(jobId => findJobById(jobId))
 
   /**
+    * Rewrite this function using for-comprehension syntax.
+    *
     * scala> findJobNameByHumanIdUsingFor(2)
     * = Some("Teacher")
     *
     * scala> findJobNameByHumanIdUsingFor(1)
     * = None
-    *
-    * Hint: Use `findJobByHumanIdUsingFor` and for comprehension
     */
-  def findJobNameByHumanIdUsingFor(humanId: HumanId): Option[String] = ???
+  def findJobNameByHumanIdUsingFor(humanId: HumanId): Option[String] =
+    findJobByHumanId(humanId).map(job => job.name)
 }
